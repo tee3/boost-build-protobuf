b48af35fc967e4ba715255fb5e5ab48a270d3890
Thomas Brown <tabsoftwareconsulting@gmail.com>
2023-09-14 15:41:55 -0400

    test: Add tests for import.

---
+++ test/tree-with-import/main.cpp
@@ -0,0 +1,15 @@
+// Copyright 2023 Thomas Brown
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+/// @file
+
+#include "company_a/protobuf/example/a0.pb.h"
+#include "company_a/protobuf/example/a1.pb.h"
+
+int
+main ()
+{
+   return 0;
+}
